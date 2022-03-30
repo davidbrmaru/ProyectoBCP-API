5c14861c5e45890bb4c7efca83821944d64c48bc
Daniela Benavides Salvatierra <danielabs4995@gmail.com>
2022-03-30 13:07:49 -0500

    .

---
+++ Models/TbAplicacion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoBCP_API.Models.Response
+{
+    public class TbAplicacion
+    {
+        private int id_Squad;
+
+        public int Id { get; set; }
+        public int Id_SquadApp { get => id_Squad; set => id_Squad = value; }
+
+        public string Cod_Aplicacion { get; set; }
+        public string Nombre { get; set; }
+        public string Cod_Owner { get; set; }
+        public string Binding_Block { get; set; }
+        public DateTime Fecha_Ingreso { get; set; }
+        public string Usuario_Ingreso { get; set; }
+        public string Usuario_Actualiza { get; set; }
+        public int Flag_Activo { get; set; }
+
+        public virtual TbAplicacion IdTAplicacionNavigation { get; set; }
+
+    }
+}
