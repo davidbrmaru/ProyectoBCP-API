6a59d85c27d0f62b97111ec61d61ace3e1bc0db7
Daniela Benavides Salvatierra <danielabs4995@gmail.com>
2022-04-07 15:34:58 -0500

    Create ApplicationController.cs
    Create ApplicationController.cs

---
+++ Controllers/ApplicationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using trabajo_final_API.Models.Response;
+using trabajo_final_API.Service;
+
+namespace trabajo_final_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationController : ControllerBase
+    {
+        private readonly IAplicacionService _aplicacionService;
+        public ApplicationController(IAplicacionService aplicacionService)
+        {
+            this._aplicacionService = aplicacionService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            WrapperResponse _response = new WrapperResponse();
+
+            try
+            {
+                _response.Data = this._aplicacionService.GetAll();
+            }
+            catch (System.Exception)
+            {
+
+                _response = null;
+            }
+
+            return Ok(_response);
+
+        }
+    }
+}
