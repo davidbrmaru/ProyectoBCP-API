7febf6b816a09051346f27ea822c6e39a8ba3aa5
Daniela Benavides Salvatierra <danielabs4995@gmail.com>
2022-04-07 16:33:40 -0500

    .

--- Controllers/ApplicationController.cs
+++ Controllers/ApplicationController.cs
@@ -27,5 +27,32 @@ namespace trabajo_final_API.Controllers
             return await _aplicacionService.GetApplication();
         }
 
+    [HttpGet("{id}")]
+        public async Task<Application> GetApplicationById(int id)
+        {
+            log.Info("Inicio Get Application By Id");
+            return await _aplicacionService.GetApplicationById(id);
+        }
+    [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] Application application)
+        {
+            log.Info("Inicio Post Application");
+            var result = await _aplicacionService.InsertApplication(application);
+            return Ok(result);
+        }
+    [HttpPut("{id}")]
+        public async Task<IActionResult> PutAsync(int id, [FromBody] Application application)
+        {
+            log.Info("Inicio Put application ById");
+            var result = await _aplicacionService.UpdateApplication(id, application);
+            return Ok(result);
+        }
+    [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id, [FromBody] Application application)
+        {
+            log.Info("Inicio Delete application ById");
+            var result = await _aplicacionService.DeleteAsyncByid(id, application);
+            return Ok(result);
+        }
     }
 }
